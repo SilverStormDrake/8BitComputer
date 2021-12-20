1d7379a438ed6cb18e2a66817e46b1f603db1284
André Luiz <andrelu0022@gmail.com>
2021-12-20 02:35:45 -0300

    Setting ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 8BitComputer
+
+8 Bit PC emulator inspired by Ben Eater "Bulding an 8-bit breadboard Computer!" youtube series
+
+Made by: André Luiz(andrelu00) 
--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /*  
-*   8 Bit PC emulator inspired by Ben Eater "Bulding an 8-bit breadboard Computer!"
+*   8 Bit PC emulator inspired by Ben Eater "Bulding an 8-bit breadboard Computer!" youtube series
 *   Made by: André Luiz(andrelu00) 
 */
 
